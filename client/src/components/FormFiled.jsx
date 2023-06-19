e9ac8785adbdbf1080af4e46b717384c3f04e90c
Kinshu-Learner <kinshurajgupta1@gmail.com>
2023-06-20 01:15:06 +0530

    Made the basic structure for CreatePost

--- client/src/components/FormFiled.jsx
+++ client/src/components/FormFiled.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const FormFiled = () => {
+const FormFiled = ({labelname, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
   return (
     <div>
-      
+      <div className="flex items-center gap-2 mb2">
+
+      </div>
     </div>
   )
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import preview from '../assets';
-import {getRandomPrompt} from '../utils';
+import { getRandomPrompt } from '../utils';
 
 import { FormFiled, Loader } from '../components';
 
@@ -19,9 +19,49 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+
   return (
-    <section className='max-w-7xl'>
-      
+    <section className='max-w-7xl mx-auto'>
+
+      <div className="">
+        <h1 className='font-extrabold text-3xl text-[#222328]'>The Create</h1>
+        <p className='text-[#666e75] mt-2'>Create imaginative and visually stunning images using DALL-E AI and share them with the community</p>
+      </div>
+
+      <form action="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormFiled
+            labelname = 'Your name'
+            type = 'text'
+            name = 'name'
+            placeholder = 'King Singh'
+            value = {form.name}
+            handleChange = {handleChange}
+          />
+          <FormFiled
+            labelname = 'Prompt'
+            type = 'text'
+            name = 'prompt'
+            placeholder = 'an oil painting by Matisse of a humanoid robot playing chess'
+            value = {form.prompt}
+            handleChange = {handleChange}
+            isSurpriseMe
+            handleSurpriseMe = {handleSurpriseMe}
+          />
+        </div>
+      </form>
+
     </section>
   )
 }
